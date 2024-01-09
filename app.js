768fd9f34cd390b70cbbd5d79b280b7c931fa0a8
Celeste Layne <layne.celeste@gmail.com>
2024-01-09 15:26:37 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log('this works');
+
+// Set authorization token.
+const searchWidget = document.querySelector('gen-search-widget');
+searchWidget.authToken = "<JWT or OAuth token provided by your backend>";
